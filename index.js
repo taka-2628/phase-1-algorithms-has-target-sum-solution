91dc88034aa7a5eda9340c7f5980d21529f93af3
Takashi <th2826@columbia.edu>
2021-09-26 12:17:33 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// SOLUTION 3 - time complexity: O(n) / space complexity: O(n)
+function hasTargetSum(array, target) {
+  const seenNumbers = new Set(); // initialize an empty Set
+  for (const number of array) {
+    const complement = target - number;
+
+    // .has returns true if the Set includes the complement
+    if (seenNumbers.has(complement)) return true;
+
+    // .add adds the number to the Set
+    seenNumbers.add(number);
+  }
+  return false;
+}
+
+/* SOLUTION 2 - time complexity: O(n) / space complexity: O(n)
 function hasTargetSum(array, target) {
   // 1 step
   const seenNumbers = {};
@@ -12,28 +28,24 @@ function hasTargetSum(array, target) {
   // 1 step
   return false;
 }
+*/
 
+/* SOLUTION 1 - time complexity: O(n²) / space complexity: O(n)
 function hasTargetSum(array, target) {
-  // create an object to keep track of all the numbers we've seen
-  const seenNumbers = {};
-  // iterate over the array of numbers
-  for (const number of array) {
-    // for the current number, identify a complementary number that adds to our target
-    // (for example: if our number is 2, and the target is 5, the complementary number is 3)
-    const complement = target - number;
-    // check if any of the keys in our object is the complement to the current number
-    // if so, return true
-    if (seenNumbers[complement]) return true;
-    // save the current number as the key on our object so we can check it later against other numbers
-    seenNumbers[number] = true;
-    console.log(seenNumbers)
+  for (let i = 0; i < array.length; i++) {
+    // n steps (depending on the length of the input array)
+    const complement = target - array[i];
+    // n * n steps (nested loop)
+    for (let j = i + 1; j < array.length; j++){
+      if (array[j] === complement){
+        return true;
+      }
+    }
   }
-  // if we reach the end of the array, return false
+  // 1 step
   return false;
 }
-
-hasTargetSum([3, 8, 12, 4, 11, 7], 10)
-
+*/
 
 /* 
   Write the Big O time complexity of your function here
@@ -47,7 +59,6 @@ hasTargetSum([3, 8, 12, 4, 11, 7], 10)
   Add written explanation of your solution here
 */
 
-/*
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
@@ -66,4 +77,3 @@ if (require.main === module) {
 }
 
 module.exports = hasTargetSum;
-*/
